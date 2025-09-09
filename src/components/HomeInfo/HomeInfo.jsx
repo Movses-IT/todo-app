55f616652397c47b26d14697b5be94c59c650e16
Movses-IT <movseskolyaevich@gmail.com>
2025-09-09 17:32:31 +0400

    Удалены все файлы

--- src/components/HomeInfo/HomeInfo.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./HomeInfo.module.css";
-
-export default function HomeInfo() {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.text}>Welcome to My Todo App.</h1>
-        </div>
-    );
-}
